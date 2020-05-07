bd682d238f43d18a92fde96f3165d1c483b6e094
Corey Lehman <coreylehman@me.com>
2020-05-07 15:01:00 -0400

    Added speedhack support for Windows.

---
+++ Source/Core/Core/HW/EXI/EXI_DeviceFrameAdvance.cpp
@@ -0,0 +1,28 @@
+// Copyright 2017 Dolphin Emulator Project
+// Licensed under GPLv2
+// Refer to the license.txt file included.
+
+#include "Core/HW/EXI/EXI_DeviceFrameAdvance.h"
+#include "Core/HW/SI/SI.h"
+
+#include <iostream>
+
+CEXIFrameAdvance::CEXIFrameAdvance() {}
+CEXIFrameAdvance::~CEXIFrameAdvance() {}
+
+void CEXIFrameAdvance::DMAWrite(u32 address, u32 size)
+{
+  std::cout << "DMAWrite(" << address << ", " << size << ")\n";
+}
+
+void CEXIFrameAdvance::DMARead(u32 address, u32 size)
+{
+  // std::cout << "DMARead(" << address << ", " << size << ")\n";
+  memory_watcher.Step();
+  SerialInterface::UpdateDevices();
+}
+
+bool CEXIFrameAdvance::IsPresent() const
+{
+  return true;
+}
